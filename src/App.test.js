553ece0a8f9c85af0f178ad4c779e5940b84c767
Geshan Manandhar <geshan@simplywallst.com>
2022-07-17 14:31:37 +1000

    Adds only a spy without mock return value for console.log

--- src/App.test.js
+++ src/App.test.js
@@ -53,7 +53,9 @@ test('should get nationalities for a name', async() => {
 });
 
 test('should handle error while getting nationalities for a name', async() => {
-  const consoleLogSpy = jest.spyOn(console, 'log').mockReturnValue(true);
+  //const consoleLogSpy = jest.spyOn(console, 'log').mockReturnValue(undefined);
+  const consoleLogSpy = jest.spyOn(console, 'log');
+
   const { container } = render(<App />);
 
   const personNameInput = screen.getByRole('textbox');
