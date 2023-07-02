c59de19a6d29ad0541f78d04bc591a5a8a54639e
Geshan Manandhar <geshan@simplywallst.com>
2023-07-02 21:32:06 +1000

    Adds new meticulous with GH actions

--- src/index.js
+++ src/index.js
@@ -3,15 +3,27 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { tryLoadAndStartRecorder } from '@alwaysmeticulous/recorder-loader'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+async function startApp() {
+  if(process.env.NODE_ENV === 'development' ||  process.env.VERCEL_ENV === 'preview') {
+    await tryLoadAndStartRecorder({
+     projectId: 'L9K2n9bouru3E81FuOa9Cg9BHXh2SkP1E39kIxEV',
+    });
+  }
+  
+  const root = ReactDOM.createRoot(document.getElementById('root'));
+  root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+  
+  // If you want to start measuring performance in your app, pass a function
+  // to log results (for example: reportWebVitals(console.log))
+  // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+  reportWebVitals();
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+startApp();
+
